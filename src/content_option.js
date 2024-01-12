b2ee9c5936463ee7d4e4822451a7b0320027f5a5
rehanshaikh55 <shaikhrehan1016@gmail.com>
2024-01-13 00:58:32 +0530

    Your message about the commit

---
+++ src/content_option.js
@@ -0,0 +1,112 @@
+const logotext = "REHAN";
+const meta = {
+    title: "Rehan",
+    description: "I'am a Full stack developer ",
+};
+
+const introdata = {
+    title: "I’m Rehan A Creative",
+    animated: {
+        first: "Full-Stack Developer",
+        second: "Mobile App Developer",
+        third: "Front-end Expert",
+    },
+    description: "  I bring a strong technical foundation and a passion for leadership.",
+    your_img_url: "https://i.ibb.co/QNqPhZb/photo.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Full-Stack developing",
+        where: "",
+        date: "2023",
+    },
+    {
+        jobtitle: "Mobile App Developing",
+        where: "",
+        date: "2019",
+    },
+ 
+];
+
+const skills = [{
+        name: "HTML,CSS,Javascript",
+        value: 90,
+    },
+    {
+        name: "React.js",
+        value: 80,
+    },
+    {
+        name: "React-native",
+        value: 80,
+    },
+    {
+        name: "Node.js",
+        value: 50,
+    },
+    {
+        name: "Wordpress",
+        value: 75,
+    },
+];
+
+const services = [{
+        title: "React.Js Website",
+        description: "",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/d0KhvM4/weather.png",
+        description: "My first html,css,javascrpit and API based web application for weather forcast",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/886HBFr/spotify.png",
+        description: " My approach to building a Spotify-like app using React Native and a backend API.",
+        link: "#",
+    },
+  
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "shaikhrehan1016@gmail.com",
+    YOUR_FONE: "+91 9173488443",
+    description: " ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_ey8W99o",
+    YOUR_TEMPLATE_ID: "32Ml6NQTLn24M88jf",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/rehanshaikh55",
+   
+    linkedin: "https://www.linkedin.com/in/rehan-shaikh-3b0822269?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app&original_referer=https%3A%2F%2Fweb.telegram.org%2F",
+    twitter: "https://twitter.com/Rehan18637186",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
